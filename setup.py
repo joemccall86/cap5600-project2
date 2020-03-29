5336c668ed62b46d8bb86f95b972346b82ecac55
Joe McCall <joemccall@intrepid-networks.com>
2020-03-29 15:48:41 -0400

    Include setuptools and pandas requirement

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Project2",
+    version="0.1",
+    packages=find_packages(),
+
+    # Project uses PANDAS. As of this writing 1.0.3 is required
+    install_requires=['pandas>=1.0.3']
+)
